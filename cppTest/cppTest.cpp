47f40bb5f627416f99dfff1bfc5e7c391f99f58e
SuKai <kai.su@harman.com>
2024-08-29 15:19:28 +0800

    add test file

---
+++ cppTest/cppTest.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+void fun(void)
+{
+
+}
+
+class CTestClass
+{
+    public:
+	    CTestClass();
+	    ~CTestClass();
+
+
+    protected:
+
+
+    private:
+        int a;
+        float b;
+
+};
+
+CTestClass::CTestClass()
+{
+}
+
+CTestClass::~CTestClass()
+{
+}
+ 
+
+
+
+int main()
+{
+    std::cout << "Hello World! \n" << std::endl; 
+
+
+
+
+    
+    cin.get();
+
+
+
+}
+ 
